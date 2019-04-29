abc28ac53de719f0ef05ba8dd3c46dbfb0f7b879
taze-fullstack <taze@fullstack.ph>
2019-04-29 21:25:04 +0800

    try axios

--- functions/one/one.js
+++ functions/one/one.js
@@ -1,12 +1,34 @@
-const faker = require('faker')
+const { RECAPTCHA_SECRET_KEY } = process.env
 
-exports.handler = (event, context, callback) => {
-  const name = faker.name.findName()
-  console.log('name', name)
-  return callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-    	data: `hi ${name}`
-    })
-  })
-}
+const axios = require('axios')
+
+const secretKey = RECAPTCHA_SECRET_KEY
+
+exports.handler = async (event, context, callback) => {
+  const captchaResponse = event.queryStringParameters.token || "NONE"
+
+  let googleUrl = 'https://www.google.com/recaptcha/api/siteverify'
+  googleUrl += '?secret=' + secretKey
+  googleUrl += '&response=' + captchaResponse
+
+  let googleHeaders = {
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
+    }
+  }
+
+  return axios.post(googleUrl, {}, googleHeaders)
+    .then(result =>
+      ({
+        statusCode: 200,
+        body: JSON.stringify(result.data)
+      })
+    )
+    .catch(error => 
+      ({
+        statusCode: 422,
+        body: String(error)
+      })
+    )
+};
+
